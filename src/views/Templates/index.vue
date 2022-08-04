7c687830b445e0fd936756cdd6990cc608fb5ebe
Abisola Odutayo <97060060+Crocheter@users.noreply.github.com>
2022-08-04 14:23:09 -0700

    just builded

--- src/views/Templates/index.vue
+++ src/views/Templates/index.vue
@@ -4,7 +4,7 @@
         <div>
             <div style="width: 300px; height: 300px; background-color: #FFFFFF; 
             box-shadow: 0px 2px 14px 1px rgba(0, 0, 0, 0.06);"></div>
-            <h3>Campus Feloowships</h3>
+            <h3>Campus Fellowships</h3>
         </div>
         </router-link>
     </div>
